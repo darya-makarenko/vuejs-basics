4cc9ff6af6ab3a0c48ec49e0b8d471c4c4ccceff
Darya <makarenko.darya.work@gmail.com>
2019-08-08 14:37:00 +0300

    enabled adding event

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <employee-table :employees="employees" />
 
     <h1>Register new employee</h1>
-    <employee-form/>
+    <employee-form @add:employee="addEmployee" />
   </div>
 </template>
 
@@ -17,6 +17,17 @@
     components: {
       EmployeeTable, 
       EmployeeForm
+    },
+    methods: {
+      addEmployee(employee) {
+        const lastId =
+          this.employees.length > 0
+            ? this.employees[this.employees.length - 1].id
+            : 0;
+        const id = lastId + 1;
+        const newEmployee = { ...employee, id };
+        this.employees = [...this.employees, employee];
+      }
     },
     data() {
       return {
